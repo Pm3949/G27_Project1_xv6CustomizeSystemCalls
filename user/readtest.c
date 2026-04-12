9caa998c1b5725408ecb46300600d67ab5f229d1
Mathew_Javadala <mathewjavadala@gmail.com>
2026-04-12 16:56:30 +0530

    Added readtest.c

--- user/readtest.c
+++ user/readtest.c
@@ -5,7 +5,7 @@ int main() {
     printf("Initial read count: %d\n", readCount());
 
     char buf[10];
-    // Perform a few read operations from stdin (fd 0)
+    // Performing a few read operations from stdin (fd 0)
     // Note: read(0, buf, 0) still triggers the syscall even if it reads 0 bytes
     read(0, buf, 0);
     read(0, buf, 0);
